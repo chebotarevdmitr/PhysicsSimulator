22fda74f3babc342517da03330425da4b91b2aa1
Dmitriy Chebotarev <4ebotarov_dim@inbox.ru>
2025-02-18 23:00:15 +0300

    modified  main.cpp

---
+++ main.cpp
@@ -0,0 +1,32 @@
+#include <SFML/Graphics.hpp>
+#include "World.hpp"
+#include "Painter.hpp"
+
+int main(int argc, char* argv[]) {
+    if (argc < 2) {
+        std::cerr << "Usage: " << argv[0] << " <world file>" << std::endl;
+        return 1;
+    }
+
+    std::string worldFilePath = argv[1];
+    World world(worldFilePath);
+
+    sf::RenderWindow window(sf::VideoMode(800, 600), "Physics Simulator");
+    Painter painter(window);
+
+    while (window.isOpen()) {
+        sf::Event event;
+        while (window.pollEvent(event)) {
+            if (event.type == sf::Event::Closed)
+                window.close();
+        }
+
+        window.clear();
+        for (const auto& ball : world.getBalls()) {
+            ball.draw(painter);
+        }
+        window.display();
+    }
+
+    return 0;
+}
