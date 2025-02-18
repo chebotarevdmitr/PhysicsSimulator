825f0e8e2cb9142086b346e5d9e0cd92fdafdb0f
Dmitriy Chebotarev <4ebotarov_dim@inbox.ru>
2025-02-18 23:30:16 +0300

    modified Painter.cpp

---
+++ Painter.cpp
@@ -0,0 +1,10 @@
+#include "Painter.hpp"
+
+Painter::Painter(sf::RenderWindow& window) : window_(window) {}
+
+void Painter::drawCircle(const Point& center, double radius, const sf::Color& color) {
+    sf::CircleShape circle(radius);
+    circle.setPosition(center.x - radius, center.y - radius);
+    circle.setFillColor(color);
+    window_.draw(circle);
+}
