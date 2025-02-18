14349ac9bfa95e94a4eee4793393cd5acb1ea598
Dmitriy Chebotarev <4ebotarov_dim@inbox.ru>
2025-02-19 01:41:33 +0300

    что-то собралось

---
+++ build/CMakeFiles/PhysicsSimulator.dir/cmake_clean.cmake
@@ -0,0 +1,17 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/PhysicsSimulator.dir/Ball.cpp.o"
+  "CMakeFiles/PhysicsSimulator.dir/Ball.cpp.o.d"
+  "CMakeFiles/PhysicsSimulator.dir/Painter.cpp.o"
+  "CMakeFiles/PhysicsSimulator.dir/Painter.cpp.o.d"
+  "CMakeFiles/PhysicsSimulator.dir/World.cpp.o"
+  "CMakeFiles/PhysicsSimulator.dir/World.cpp.o.d"
+  "CMakeFiles/PhysicsSimulator.dir/main.cpp.o"
+  "CMakeFiles/PhysicsSimulator.dir/main.cpp.o.d"
+  "PhysicsSimulator"
+  "PhysicsSimulator.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/PhysicsSimulator.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
