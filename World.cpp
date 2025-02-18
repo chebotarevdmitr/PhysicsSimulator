57a0a62d91ae76414ea3c1a0ce777e7af75c6693
Dmitriy Chebotarev <4ebotarov_dim@inbox.ru>
2025-02-18 23:24:34 +0300

    modified World.cpp

---
+++ World.cpp
@@ -0,0 +1,33 @@
+#include "World.hpp"
+#include <fstream>
+#include <sstream>
+
+World::World(const std::string& worldFilePath) {
+    std::ifstream stream(worldFilePath);
+    if (!stream.is_open()) {
+        throw std::runtime_error("Failed to open world file");
+    }
+
+    double x, y, vx, vy;
+    int red, green, blue;
+    double radius;
+    bool isCollidable;
+
+    while (stream.good()) {
+        stream >> x >> y >> vx >> vy;
+        stream >> red >> green >> blue;
+        stream >> radius;
+        stream >> std::boolalpha >> isCollidable;
+
+        Point center(x, y);
+        Velocity velocity(vx, vy);
+        sf::Color color(red, green, blue);
+
+        Ball ball(center, velocity, radius, radius * radius, color);
+        balls_.push_back(ball);
+    }
+}
+
+const std::vector<Ball>& World::getBalls() const {
+    return balls_;
+}
